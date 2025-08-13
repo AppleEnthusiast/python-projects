98c4662ac5a96ea86dc9f15a94e25aaf5cf69c08
Teoman Dogueri <teo_dogu@yahoo.de>
2025-08-13 19:51:07 +0200

    Update hello_world.py Created function hello_world()

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,8 @@
 # this is my first file on the repository
 
-print("Hello World")
-print("I'm happy starting over again with Python.")
+def hello_world():
+	print("Hello World")
+	print("I'm happy starting over with Python.")
+
+
+hello_world()
