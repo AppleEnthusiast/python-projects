97676e1a4916d7bd5f6b76b1da4d43194e2b7cab
Teoman Dogueri <Teoman.dogueri@icloud.com>
2025-08-19 17:21:06 +0200

    Add a comment which explains that X in replace-method is a placeholder

--- format_prices.py
+++ format_prices.py
@@ -4,5 +4,7 @@ quantity = 30
 total = price * quantity
 
 print(f"us-format: ${total:,.2f}")
+
+# X in replace is a placeholder
 print(f"de-format: {total:,.2f} €".replace(",","X").replace(".",",").replace("X","."))
 
