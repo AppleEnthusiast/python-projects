1ba91db73aae10e6ee9c0ce4e2a491435b9bc52c
Teoman Dogueri <Teoman.dogueri@icloud.com>
2025-08-21 14:41:12 +0200

    Update progress_calculator.py
    Change progress_calculator.py so that it specifically calculates reading progress
    Ask user how many pages he wants to read
    Ask user how many pages he has already read
    Display his progress in percent

--- progress_calculator.py
+++ progress_calculator.py
@@ -1,15 +1,16 @@
 # Simple program: Show progress in percent
 
 print("Welcome to the Progress Calculator!")
+print("This program calculates your reading progress.")
 
 # Ask for the total goal and the current progress
-goal = int(input("What is your goal? (e.g. number of pages, euros, kilometers): "))
-progress = int(input("How much have you already achieved? "))
+goal = int(input("How many pages do you want to read?")
+progress = int(input("How many pages have you already read? "))
 
 # Calculate share
 share = progress / goal
 
 # Output
-print(f"You have completed {progress} out of {goal}.")
+print(f"You have read {progress} out of {goal}.")
 print(f"That is {share:.1%} of your goal.")
 
