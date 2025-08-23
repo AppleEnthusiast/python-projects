fee98d75863dba6f3def807ab07a7a47bc6c7094
Teoman Dogueri <Teoman.dogueri@icloud.com>
2025-08-23 09:18:37 +0200

    Create multiline.py
    Ask user to input his name
    Define formatted multiline string msg to greet user by his name
    Add print statement to display greeting

---
+++ multiline.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+
+msg = f"""
+*********************************************************
+                 Welcome, {name}!                        
+*********************************************************
+
+Nice to see you here, {name}.
+Let's start programming in Python!
+"""
+
+print(msg)
+
