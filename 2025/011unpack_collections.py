eb9fe0fdb2229ddf4d3c7384c05a50b519cd2f3e
AppleEnthusiast <Teoman.dogueri@icloud.com>
2025-09-27 15:43:02 +0200

    Create script to demonstrate list and tuple unpacking

---
+++ 2025/011unpack_collections.py
@@ -0,0 +1,12 @@
+cities = ["Hamburg","London","New York"]
+cars = ("Mercedes","BMW","Ferrari")
+
+city_1,city_2,city_3 = cities
+car_1,car_2,car_3 = cars
+
+print(city_1)
+print(city_2)
+print(city_3)
+print(car_1)
+print(car_2)
+print(car_3)
