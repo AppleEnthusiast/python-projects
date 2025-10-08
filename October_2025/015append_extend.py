5e2359c589b816fe026077c5ba68083de1921b21
AppleEnthusiast <Teoman.dogueri@icloud.com>
2025-10-08 16:10:51 +0200

    Create script that shows difference between append() and extend() methods

---
+++ October_2025/015append_extend.py
@@ -0,0 +1,12 @@
+numbers = [1,2,3]
+numbers_2 = [4,5,6]
+numbers_3= [7,8,9]
+
+numbers.extend(numbers_2)
+print("after extend():")
+print(numbers)
+
+numbers.append(numbers_3)
+print("after append():")
+print(numbers)
+
