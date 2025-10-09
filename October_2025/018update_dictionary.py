658997b5dad4a99b8fa92dfb2ee0fd8427f0e389
AppleEnthusiast <Teoman.dogueri@icloud.com>
2025-10-09 15:41:32 +0200

    Create script that shows how to merge two dictionaries

---
+++ October_2025/018update_dictionary.py
@@ -0,0 +1,13 @@
+person = {
+	"name":"Lisa",
+	"city":"London"
+}
+new_data = {
+	"city":"New York",
+	"profession": "Dancer"
+}
+
+print("Original Data:",person)
+person.update(new_data)
+print("After update:",person)
+
