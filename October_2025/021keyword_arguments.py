d8ca193ae006560279a1f4fab96f77766a041485
AppleEnthusiast <Teoman.dogueri@icloud.com>
2025-10-10 14:39:25 +0200

    Create script demonstration how to use keyword arguments in a function call

---
+++ October_2025/021keyword_arguments.py
@@ -0,0 +1,5 @@
+def route(start,destination,transport="car",fastest_route=True):
+	print(f"From {start} to {destination} by {transport}. Fastest route: {fastest_route}")
+
+route(destination="Munich",start="Hamburg",transport="train")
+
